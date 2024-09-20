72f5f4b5cc44a5e4b90d9590dc8463bd6f171979
nvalenne-iut90 <nathan.valenne@gmail.com>
2024-09-20 10:45:22 +0200

    ajout de la liste des produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,15 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
 
 class ProductController extends AbstractController
 {
     #[Route('admin/products', name: 'app_product')]
-    public function index(): Response
+    public function index(ProductRepository $repository): Response
     {
+        $products = $repository->findAll();
+        // dd($products);
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
-            'products' => [],
+            'products' => $products,
         ]);
     }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -31,4 +31,40 @@ class Product
         return $this->id;
     }
 
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
 }
